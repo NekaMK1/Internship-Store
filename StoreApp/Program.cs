3e9423e7ffb7aef490e43607a57d6754645d4c59
nemanja6916@its.edu.rs <nemanja6916@its.edu.rs>
2022-01-25 17:57:16 +0100

    Basic Classes

--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -10,6 +10,56 @@ namespace StoreApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine();
+        }
+    }
+    class Product{
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public float Price { get; set; }
+        public Product(string name,string brand,float price)
+        {
+            Name = name;
+            Brand = brand;
+            Price = price;
+        }
+    }
+    class Perishables : Product
+    {
+        public DateTime ExpirationDate { get; set; }
+        public Perishables(string name, string brand, float price, DateTime expirationDate) : base(name, brand, price)
+        {
+            ExpirationDate = expirationDate;
+        }
+    }
+    class Food : Perishables
+    {
+        public Food(string name, string brand, float price, DateTime expirationDate) : base(name, brand, price, expirationDate) { }
+    }
+    class Beverage : Perishables
+    {
+        public Beverage(string name, string brand, float price, DateTime expirationDate) : base(name, brand, price, expirationDate) { }
+    }
+    class Clothes : Product
+    {
+        public string Size { get; set; }
+        public string Color { get; set; }
+        public Clothes(string name, string brand, float price, string size,string color) : base(name, brand, price)
+        {
+            Size = size;
+            Color = color;
+        }
+    }
+    class Applience : Product
+    {
+        public string Model { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public float Weight { get; set; }
+        public Applience(string name, string brand, float price, string model,DateTime productionDate,float weight) : base(name, brand, price)
+        {
+            Model = model;
+            ProductionDate = productionDate;
+            Weight = weight;
         }
     }
 }
